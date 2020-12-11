dfc070359535d24e2dfc01f1aef4884b33dba673
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-11 17:48:13 -0500

    commit tree 4bea9097e6d3345f9115cbc6ce8442c94c8d32b4

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require "pry"
+
+def prime?(number)
+    return false if number <= 1  
+    options = (2...number).to_a
+    options.none? { |n| number % n == 0}
+end
+
+prime?(10)
+prime?(-10)
+prime?(5)
+
